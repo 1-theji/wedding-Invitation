e7be032339fd4f9fa23c3a86ba4bebb0d9428ab8
JiSeungHoon <jish0516>
2024-09-07 16:46:47 +0900

    사진변수 수정

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,11 +3,24 @@ import ImageGallery from "react-image-gallery";
 import { Divider } from "antd";
 import styled from "styled-components";
 
-import snapImage01 from "../assets/snapImage01.png";
-import snapImage02 from "../assets/snapImage02.png";
-import snapImage03 from "../assets/snapImage03.png";
-import snapImage04 from "../assets/snapImage04.png";
-import snapImage05 from "../assets/snapImage05.png";
+import IMG01 from "../assets/IMG01.png";
+import IMG02 from "../assets/IMG02.png";
+import IMG03 from "../assets/IMG03.png";
+import IMG04 from "../assets/IMG04.png";
+import IMG05 from "../assets/IMG05.png";
+import IMG06 from "../assets/IMG06.png";
+import IMG07 from "../assets/IMG07.png";
+import IMG08 from "../assets/IMG08.png";
+import IMG09 from "../assets/IMG09.png";
+import IMG10 from "../assets/IMG10.png";
+import IMG11 from "../assets/IMG11.png";
+import IMG12 from "../assets/IMG12.png";
+import IMG13 from "../assets/IMG13.png";
+import IMG14 from "../assets/IMG14.png";
+import IMG15 from "../assets/IMG15.png";
+import IMG16 from "../assets/IMG16.png";
+import IMG17 from "../assets/IMG17.png";
+import IMG18 from "../assets/IMG18.png";
 
 
 const Wrapper = styled.div`
@@ -27,25 +40,79 @@ const Title = styled.p`
 
 const images = [
   {
-    original: snapImage01,
-    thumbnail: snapImage01,
+    original: IMG01,
+    thumbnail: IMG01,
   },
   {
-    original: snapImage02,
-    thumbnail: snapImage02,
+    original: IMG02,
+    thumbnail: IMG02,
   },
   {
-    original: snapImage03,
-    thumbnail: snapImage03,
+    original: IMG03,
+    thumbnail: IMG03,
   },
   {
-    original: snapImage04,
-    thumbnail: snapImage04,
+    original: IMG04,
+    thumbnail: IMG04,
   },
   {
-    original: snapImage05,
-    thumbnail: snapImage05,
-  }
+    original: IMG05,
+    thumbnail: IMG05,
+  },
+  {
+    original: IMG06,
+    thumbnail: IMG06,
+  },
+  {
+    original: IMG07,
+    thumbnail: IMG07,
+  },
+  {
+    original: IMG08,
+    thumbnail: IMG08,
+  },
+  {
+    original: IMG09,
+    thumbnail: IMG09,
+  },
+  {
+    original: IMG10,
+    thumbnail: IMG10,
+  },
+  {
+    original: IMG11,
+    thumbnail: IMG11,
+  },
+  {
+    original: IMG12,
+    thumbnail: IMG12,
+  },
+  {
+    original: IMG13,
+    thumbnail: IMG13,
+  },
+  {
+    original: IMG14,
+    thumbnail: IMG14,
+  },
+  {
+    original: IMG15,
+    thumbnail: IMG15,
+  },
+  {
+    original: IMG16,
+    thumbnail: IMG16,
+  },
+  {
+    original: IMG17,
+    thumbnail: IMG17,
+  },
+  {
+    original: IMG18,
+    thumbnail: IMG18
+  },
+  
+  
 ];
 
 const Gallery = () => {
