670c2c034c18a51e67213a6079f56a7fcf3ac524
JiSeungHoon <jish0516>
2024-09-07 18:07:10 +0900

    사진수정

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -7,23 +7,24 @@ import IMG01 from "../assets/IMG01.jpg";
 import IMG02 from "../assets/IMG02.jpg";
 import IMG03 from "../assets/IMG03.jpg";
 import IMG04 from "../assets/IMG04.jpg";
-import IMG05 from "../assets/IMG05크기수정.jpg";
+import IMG05 from "../assets/IMG05_크기수정.jpg";
 import IMG06 from "../assets/IMG06.jpg";
 import IMG07 from "../assets/IMG07.jpg";
 import IMG08 from "../assets/IMG08.jpg";
 import IMG09 from "../assets/IMG09.jpg";
 import IMG10 from "../assets/IMG10.jpg";
 import IMG11 from "../assets/IMG11.jpg";
-import IMG12 from "../assets/IMG12크기수정.jpg";
-import IMG13 from "../assets/IMG13크기수정.jpg";
+import IMG12 from "../assets/IMG12_크기수정.jpg";
+import IMG13 from "../assets/IMG13_크기수정.jpg";
 import IMG14 from "../assets/IMG14.jpg";
 import IMG15 from "../assets/IMG15.jpg";
 import IMG16 from "../assets/IMG16.jpg";
-import IMG17 from "../assets/IMG17크기수정.jpg";
-import IMG18 from "../assets/IMG18크기수정.jpg";
+import IMG17 from "../assets/IMG17_크기수정.jpg";
+import IMG18 from "../assets/IMG18_크기수정.jpg";
 
 
 const Wrapper = styled.div`
+  position: relative;
   padding-top: 42px;
   width: 70%;
   margin: 0 auto;
@@ -56,8 +57,8 @@ const images = [
     thumbnail: IMG04,
   },
   {
-    original: IMG05크기수정,
-    thumbnail: IMG05크기수정,
+    original: IMG05,
+    thumbnail: IMG05,
   },
   {
     original: IMG06,
@@ -84,12 +85,12 @@ const images = [
     thumbnail: IMG11,
   },
   {
-    original: IMG12크기수정,
-    thumbnail: IMG12크기수정,
+    original: IMG12,
+    thumbnail: IMG12,
   },
   {
-    original: IMG13크기수정,
-    thumbnail: IMG13크기수정,
+    original: IMG13,
+    thumbnail: IMG13,
   },
   {
     original: IMG14,
@@ -104,18 +105,31 @@ const images = [
     thumbnail: IMG16,
   },
   {
-    original: IMG17크기수정,
-    thumbnail: IMG17크기수정,
+    original: IMG17,
+    thumbnail: IMG17,
   },
   {
-    original: IMG18크기수정,
-    thumbnail: IMG18크기수정
+    original: IMG18,
+    thumbnail: IMG18
   },
   
   
 ];
 
 const Gallery = () => {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [zoomedImage, setZoomedImage] = useState(null);
+
+  const handleImageClick = (index) => {
+    setIsZoomed(true);
+    setZoomedImage(images[index].original);
+  };
+
+  const handleCloseZoom = () => {
+    setIsZoomed(false);
+    setZoomedImage(null);
+  };
+
   return (
     <Wrapper>
       <Divider style={{ marginTop: 0, marginBottom: 32 }} plain>
@@ -125,7 +139,19 @@ const Gallery = () => {
         showPlayButton={false}
         showFullscreenButton={false}
         items={images}
+        onThumbnailClick={(event, index) => handleImageClick(index)}
       />
+
+    {isZoomed && (
+        <div className="overlay" onClick={handleCloseZoom}>
+          <img 
+            src={zoomedImage} 
+            alt="zoomed"
+            className="zoomed-image"
+          />
+          <span className="close" onClick={handleCloseZoom}>&times;</span>
+        </div>
+      )}
     </Wrapper>
   );
 };
